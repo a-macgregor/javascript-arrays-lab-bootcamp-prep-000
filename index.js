2d11d40f59068d8b3b8856961b2c8bdad3045f79
A MacGregor <annabel.macgregor@gmail.com>
2018-03-21 20:53:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,31 +2,37 @@ const app = "I don't do much.";
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
+//1 
 function destructivelyAppendKitten(name) {
   kittens.push(`${name}`);
   return kittens;
 }
 
+//2 
 function destructivelyPrependKitten(name) {
   kittens.unshift(`${name}`);
   return kittens;
 }
 
+//3 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 }
 
+//4 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
 
+//5
 function appendKitten(name) {
-  [...kittens, ${name}];
+  kittens = [...kittens, ${name}];
   return kittens;
 }
 
+//6 
 function prependKitten(name) {
   kittens = kittens.unshift(`${name}`);
   return kittens;
